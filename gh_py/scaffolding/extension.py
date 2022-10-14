0f2b7ca31b31306c8ca75414b342bc82f83b9331
JessicaTegner <jessica.tegner@outlook.com>
2022-10-14 12:49:33 +0200

    Updated the scaffolding to be much more modern

--- gh_py/scaffolding/extension.py
+++ gh_py/scaffolding/extension.py
@@ -1,5 +1,28 @@
-import argparse
+import sys
 
-def run():
-    parser = argparse.ArgumentParser(prog='My GH Extension')
-    result = parser.parse_args()
+from cleo.application import Application
+from cleo.io.io import IO
+from cleo.io.inputs.string_input import StringInput
+from cleo.commands.command import Command
+
+def run(args=None):
+    # Set your name and version here.
+    application = Application(name='My Awesome GH Extension', version='0.1.0')
+    # add your commands here.
+    application.add(HelloCommand())
+
+    # below here we run. You don't need to modify this.
+    if args:
+        input = StringInput(args)
+        input.set_stream(sys.stdin)
+        application.run(input)
+    else:
+        application.run()
+
+class HelloCommand(Command):
+    name = "hello"
+    description = "Say hello to the console."
+
+    def handle(self):
+        self.line('Hello World')
+    
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import shutil
+import math
 
 from pathlib import Path
 
@@ -43,5 +44,37 @@ def test_extension_dir_before_and_after():
 	subprocess.run(["sh", "gh-test", "-h"], shell=False)
 	assert Path(".gh-py").exists()
 	assert Path("poetry.lock").exists()
+	os.chdir("../")
+	shutil.rmtree("gh-test")
+
+def test_update_of_extension_env():
+	subprocess.run(["gh", "py", "create", "gh-test"])
+	os.chdir("gh-test")
+	assert not Path(".gh-py").exists()
+	assert not Path("poetry.lock").exists()
+	subprocess.run(["sh", "gh-test", "-h"], shell=False)
+	assert Path(".gh-py").exists()
+	assert Path("poetry.lock").exists()
+	os.remove("poetry.lock")
+	assert not Path("poetry.lock").exists()
+	subprocess.run(["sh", "gh-test", "-h"], shell=False)
+	assert Path("poetry.lock").exists()
+	os.chdir("../")
+	shutil.rmtree("gh-test")
+
+def test_with_newer_pyproject():
+	subprocess.run(["gh", "py", "create", "gh-test"])
+	os.chdir("gh-test")
+	assert not Path(".gh-py").exists()
+	assert not Path("poetry.lock").exists()
+	subprocess.run(["sh", "gh-test", "-h"], shell=False)
+	assert Path(".gh-py").exists()
+	assert Path("poetry.lock").exists()
+	# Change last modified day of pyproject.toml to be newer than poetry.lock
+	os.utime("pyproject.toml", (os.path.getatime("pyproject.toml"), os.path.getmtime("poetry.lock") + 1))
+	# call the extension again.
+	subprocess.run(["sh", "gh-test", "-h"], shell=False)
+	# check that poetry.lock is newer than pyproject.toml, comparing the float numbers with the math module, with a precision of 1 decimal.
+	assert math.isclose(os.path.getmtime("poetry.lock"), os.path.getmtime("pyproject.toml"), rel_tol=1e-1)
 	os.chdir("../")
 	shutil.rmtree("gh-test")
